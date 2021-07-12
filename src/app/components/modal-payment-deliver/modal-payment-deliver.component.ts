d4e33d10c5ae1699d9bed3a3197d96f153a49f9a
Katherine-fe <kattyserranobasan@gmail.com>
2021-07-11 22:45:12 -0500

    instalando bootstrap y estructura components

---
+++ src/app/components/modal-payment-deliver/modal-payment-deliver.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-payment-deliver',
+  templateUrl: './modal-payment-deliver.component.html',
+  styleUrls: ['./modal-payment-deliver.component.scss']
+})
+export class ModalPaymentDeliverComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
