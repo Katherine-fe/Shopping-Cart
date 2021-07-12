7f2ac2ea9a97ed2fc2becc788342cae61ba7dcd0
Yessenia <vela.kathy@gmail.com>
2021-07-12 10:53:50 -0500

    dependencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AngularFireModule  } from '@angular/fire'
+import { AngularFireModule  } from '@angular/fire';
+import { AngularFirestoreModule } from "@angular/fire/firestore";
 import { environment } from '../environments/environment';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
