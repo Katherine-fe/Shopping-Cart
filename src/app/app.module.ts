3e736bf7cd1d07b02e25ffbcee0eaa88924089b1
Yessenia <vela.kathy@gmail.com>
2021-07-12 10:48:03 -0500

    estructura para el aplicativo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,21 @@ import { AppComponent } from './app.component';
 import { AngularFireModule  } from '@angular/fire'
 import { environment } from '../environments/environment';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { FooterComponent } from './components/footer/footer.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
-import { ModalProductComponent } from './components/modal-product/modal-product.component';
-import { ModalPaymentDeliverComponent } from './components/modal-payment-deliver/modal-payment-deliver.component';
-import { MainComponent } from './view/main/main.component';
 import { ProductsComponent } from './view/products/products.component';
 import { CartComponent } from './view/cart/cart.component';
+import { LoginComponent } from './view/login/login.component';
+import { ModalComponent } from './components/modal/modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    FooterComponent,
     ProductCardComponent,
-    ModalProductComponent,
-    ModalPaymentDeliverComponent,
-    MainComponent,
     ProductsComponent,
-    CartComponent
+    CartComponent,
+    LoginComponent,
+    ModalComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/components/modal/modal.component.spec.ts
+++ src/app/components/modal/modal.component.spec.ts
@@ -1,20 +1,20 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ModalProductComponent } from './modal-product.component';
+import { ModalComponent } from './modal.component';
 
-describe('ModalProductComponent', () => {
-  let component: ModalProductComponent;
-  let fixture: ComponentFixture<ModalProductComponent>;
+describe('ModalComponent', () => {
+  let component: ModalComponent;
+  let fixture: ComponentFixture<ModalComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ModalProductComponent ]
+      declarations: [ ModalComponent ]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(ModalProductComponent);
+    fixture = TestBed.createComponent(ModalComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
