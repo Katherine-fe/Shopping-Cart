4ff425128153ad122631c013b07b47a0087591e6
Katherine-fe <kattyserranobasan@gmail.com>
2021-07-12 15:43:16 -0500

    modales

--- src/app/components/modal/modal.component.ts
+++ src/app/components/modal/modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit , Input,EventEmitter,Output} from '@angular/core';
 
 @Component({
   selector: 'app-modal',
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ModalComponent implements OnInit {
 
   constructor() { }
+
 
   ngOnInit(): void {
   }
